57f93738a7ff6a9269bbeefee4fcbd3505b5bd48
Jason Jacobs <nerfnerd@gmail.com>
2022-07-03 21:26:12 -0700

    left out } causing build not to work

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -12,75 +12,75 @@ import mixpanel from "mixpanel-browser";
 mixpanel.init(process.env.MIXPANEL_API_KEY)
 
 const Blog = ({ post, router }) => {
-  useEffect(() => {
-    const res = mixpanel.track('VISIT_BLOGPOST', {
-      title: post.content.title,
-      slug: post.slug
-    });
+    useEffect(() => {
+        const res = mixpanel.track('VISIT_BLOGPOST', {
+            title: post.content.title,
+            slug: post.slug
+        });
 
-    console.log(res);
-  }, []);
+        console.log(res);
+    }, []);
 
-  return (
-  <>
-    {post !== null && post !== undefined && (
-      <HeadSeo
-        title="Volume Finance"
-        description=""
-        content={post.content}
-      />
-    )}
-    <div className="page-container">
-      {post !== null && post !== undefined && (
-        <div className="blog-page-container">
-          <div className="blog-post-title-bg">
-            <div className="blog-post-title-wrapper">
-              <h1 className="blog-title">{post.content.title}</h1>
-            </div>
-          </div>
-          <div className="blog-post-view">
-            <div className="blog-content">
+    return (
+        <>
+            {post !== null && post !== undefined && (
+                <HeadSeo
+                    title="Volume Finance"
+                    description=""
+                    content={post.content}
+                />
+            )}
+            <div className="page-container">
+                {post !== null && post !== undefined && (
+                    <div className="blog-page-container">
+                        <div className="blog-post-title-bg">
+                            <div className="blog-post-title-wrapper">
+                                <h1 className="blog-title">{post.content.title}</h1>
+                            </div>
+                        </div>
+                        <div className="blog-post-view">
+                            <div className="blog-content">
 
 
-            </div>
-            {/* <div className="blog-divider"></div> */}
-            <div className="blog-pubtime">
-                {convertDateString2(post.first_published_at)}
-              </div>
-            <div className="blog-post">
-              {render(post.content.long_text, {
-                nodeResolvers: {
-                  [NODE_IMAGE]: (children, props) => (
-                    <img
-                      {...props}
-                      style={{ borderRadius: "0px", maxWidth: "100%" }}
-                    />
-                  ),
-                },
-                blokResolvers: {
-                  ["YouTube-blogpost"]: (props) => (
-                    <div className="embed-responsive embed-responsive-16by9">
-                      <iframe
-                        className="embed-responsive-item"
-                        src={
-                          "https://www.youtube.com/embed/" +
-                          props.YouTube_id.replace("https://youtu.be/", "")
-                        }
-                      ></iframe>
+                            </div>
+                            {/* <div className="blog-divider"></div> */}
+                            <div className="blog-pubtime">
+                                {convertDateString2(post.first_published_at)}
+                            </div>
+                            <div className="blog-post">
+                                {render(post.content.long_text, {
+                                    nodeResolvers: {
+                                        [NODE_IMAGE]: (children, props) => (
+                                            <img
+                                                {...props}
+                                                style={{borderRadius: "0px", maxWidth: "100%"}}
+                                            />
+                                        ),
+                                    },
+                                    blokResolvers: {
+                                        ["YouTube-blogpost"]: (props) => (
+                                            <div className="embed-responsive embed-responsive-16by9">
+                                                <iframe
+                                                    className="embed-responsive-item"
+                                                    src={
+                                                        "https://www.youtube.com/embed/" +
+                                                        props.YouTube_id.replace("https://youtu.be/", "")
+                                                    }
+                                                ></iframe>
+                                            </div>
+                                        ),
+                                    },
+                                })}
+                            </div>
+                        </div>
                     </div>
-                  ),
-                },
-              })}
+                )}
             </div>
-          </div>
-        </div>
-      )}
-    </div>
-  </>
-);
-
+        </>
+    );
+}
 // This function gets called at build time
-export async function getStaticPaths() {
+ async function getStaticPaths() {
   // Call an external API endpoint to get posts
   const posts = await fetchBlogs();
 
@@ -95,7 +95,7 @@ export async function getStaticPaths() {
 }
 
 // This also gets called at build time
-export async function getStaticProps({ params }) {
+ async function getStaticProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
   const posts = await fetchBlogs();
