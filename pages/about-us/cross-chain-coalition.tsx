4024f5d11e6a321c6bc0375a8773c643d87dba4c
wdev733 <wdev733@gmail.com>
2022-07-29 10:18:22 -0400

    fix ccc-about page header text

--- pages/about-us/cross-chain-coalition.tsx
+++ pages/about-us/cross-chain-coalition.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import { render, NODE_IMAGE } from "storyblok-rich-text-react-renderer";
 
 import SubscribeForm from "components/SubscribeForm";
 
-import {   CCC_TELEGRAM, CCC_PARTNERS, STORYBLOK_PAGES } from "utils/constants";
+import { CCC_TELEGRAM, CCC_PARTNERS, STORYBLOK_PAGES } from "utils/constants";
 import { fetchPageValues } from "utils/storyblok";
 
 const Pillar = ({ data }) => (
@@ -30,13 +31,15 @@ export default function CrossChainCoalition({ state, router }) {
       }
 
       const content = homeData.content;
+      console.log(content);
 
       const data = {
         pageMainTitle: content.title,
         pageSubTitle: content.subtitle,
 
         pageHeaderTitle: content.header,
-        pageHeaderText: content.text,
+        pageHeaderTextLeft: content.text_left,
+        pageHeaderTextRight: content.text_right,
 
         pillars: [
           {
@@ -57,7 +60,7 @@ export default function CrossChainCoalition({ state, router }) {
             title: content.pillar_subtitle3,
             description: content.pillar_text3,
           },
-        ]
+        ],
       };
 
       setData(data);
@@ -78,12 +81,8 @@ export default function CrossChainCoalition({ state, router }) {
             <img className="top-logo ccc" src="/assets/logo/ccc2.png" />
           </div>
           <div className="top-content">
-            <div className="top-content-title">
-              {data.pageMainTitle}
-            </div>
-            <div className="top-content-description">
-              {data.pageSubTitle}
-            </div>
+            <div className="top-content-title">{data.pageMainTitle}</div>
+            <div className="top-content-description">{data.pageSubTitle}</div>
             <a href={CCC_TELEGRAM} target="_blank" className="launch-link">
               join the cross chain coalition
             </a>
@@ -93,23 +92,29 @@ export default function CrossChainCoalition({ state, router }) {
         <div className="ccc-history">
           <h2 className="ccc-history-title">{data.pageHeaderTitle}</h2>
           <div className="ccc-history-content">
-            {/* {data.pageHeaderText} */}
             <div className="ccc-history-left">
-              The Cross Chain Coalition (CCC) is a collective of builders that
-              have united together to educate developers on cross chain
-              infrastructure. <br/><br/>The CCC aims to expand awareness and increase
-              education for cross chain development. join us in our journey to
-              turn every blockchain engineer into a cross chain engineer.
+              {render(data.pageHeaderTextLeft, {
+                nodeResolvers: {
+                  [NODE_IMAGE]: (children, props) => (
+                    <img
+                      {...props}
+                      style={{ borderRadius: "0px", maxWidth: "100%" }}
+                    />
+                  ),
+                },
+              })}
             </div>
             <div className="ccc-history-right">
-              The CCC was founded in May of 2022 by a collection of crypto
-              innovators, builders, and investors spanning from various chains
-              and ventures.<br/><br/> The current digital asset ecosystem is fragmented by
-              chains and tribes all competing for the same future. As
-              invigorating as competition can be, we think intra-ecosystem
-              competition is ultimately inefficient. If we want to bring the
-              world to crypto, we must first bring crypto to the world together.
-              The future of crypto is cross chain.
+              {render(data.pageHeaderTextRight, {
+                nodeResolvers: {
+                  [NODE_IMAGE]: (children, props) => (
+                    <img
+                      {...props}
+                      style={{ borderRadius: "0px", maxWidth: "100%" }}
+                    />
+                  ),
+                },
+              })}
             </div>
           </div>
         </div>
@@ -120,7 +125,13 @@ export default function CrossChainCoalition({ state, router }) {
               {CCC_PARTNERS.map((img, index) => (
                 <img
                   src={img}
-                  style={{ opacity: img.endsWith("figment.png") || img.endsWith("simple-vc.png") ? 0.7 : 1 }}
+                  style={{
+                    opacity:
+                      img.endsWith("figment.png") ||
+                      img.endsWith("simple-vc.png")
+                        ? 0.7
+                        : 1,
+                  }}
                   className="volume-investor-logo"
                   key={`volume-investor-${index}`}
                 />
