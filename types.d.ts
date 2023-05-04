d60a8a02d8a38e3956f1ee200ae8af8eb193d7e1
wdev733 <wdev733@gmail.com>
2023-05-04 05:17:34 -0400

    remove types

--- types.d.ts
+++ types.d.ts
@@ -1,38 +1,3 @@
 interface DefaultComponentProps  {
   className?: string;
 }
-
-export type TMap = {
-  [key: string]: any;
-};
-
-export type LIQUIDITY_BUTTON_STATUS =
-  | "success"
-  | "enter_amount"
-  | "insufficient"
-  | "loading";
-
-export type LIQUIDITY_BUTTON_TEXT =
-  | "Deposit UST"
-  | "Enter an amount"
-  | "Insufficient Balance"
-  | "Confirm Withdraw"
-  | "Insufficient Liquidity"
-  | "Withdrawing UST";
-
-export interface LIQUIDITY_BALANCE_STATUS {
-  text: LIQUIDITY_BUTTON_TEXT;
-  status: LIQUIDITY_BUTTON_STATUS;
-}
-
-export type COLLECT_TYPE = "UST" | "bLUNA";
-
-export type TRANSACTION_STATUS =
-  | "Success"
-  | "User Denied"
-  | "Create Tx Failed"
-  | "Tx Failed"
-  | "Timeout"
-  | "Unspecified Error"
-  | "Unknown Error";
-
