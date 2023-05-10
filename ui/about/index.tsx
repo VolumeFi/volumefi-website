37ba9458ffeff0f442ff1acd20183ce15623dd8b
wdev733 <wdev733@gmail.com>
2023-05-10 11:24:09 -0400

    update pricing page

--- ui/about/index.tsx
+++ ui/about/index.tsx
@@ -3,21 +3,19 @@ import style from 'ui/about/index.module.scss';
 import Intro from 'ui/about/Intro';
 import PrivateKeyManagement from 'ui/about/PrivateKeyManagement';
 import ProductsAndService from 'ui/about/ProductsAndService';
-import TotalTransactions from 'ui/about/TotalTransactions';
 import Panel from 'ui/common/Panel';
 
 const AboutContainer = () => (
   <section className={style.container}>
     <Intro />
     <ProductsAndService />
-    <TotalTransactions />
+    {/* <TotalTransactions /> */}
     <PrivateKeyManagement />
     <CompareServices />
     <Panel
       title={
         <>
-          Start building now{' '}
-          <br />
+          Start building now <br />
           using Volume SDK
         </>
       }
