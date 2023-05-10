f026b4ba6f9f553cc5376862a11cf059223b0f34
wdev733 <wdev733@gmail.com>
2023-05-10 02:45:26 -0400

    fix typo

--- ui/pricing/Intro.tsx
+++ ui/pricing/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => (
   <SectionContainer>
     <section className={style.container}>
       <div className={style.textWrapper}>
-        <div className={style.sub}>Your keys, your money.</div>
+        <div className={style.sub}>Your keys, your money</div>
         <h1 className={style.title}>Start Messaging Securely Now</h1>
         <p className={style.description}>Contact us for a customized plan if you are ready to scale.</p>
       </div>
