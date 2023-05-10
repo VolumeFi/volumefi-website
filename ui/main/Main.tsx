f9d8e783d4f39412bb4cd25afc35f29eadd4e246
wdev733 <wdev733@gmail.com>
2023-05-10 16:26:48 -0400

    fix site title

--- ui/main/Main.tsx
+++ ui/main/Main.tsx
@@ -5,17 +5,27 @@ import Header from 'ui/header';
 import style from 'ui/main/Main.module.scss';
 
 import type { Router } from 'next/router';
+import Head from 'next/head';
 
 interface MainContainerProps {
   router: Router;
 }
 
 const MainContainer = ({ children }: PropsWithChildren<MainContainerProps>) => (
-  <section className={style.container}>
-    <Header />
-    {children}
-    <Footer />
-  </section>
+  <>
+    <Head>
+      <title>Volume</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+    </Head>
+    <main>
+      <section className={style.container}>
+        <Header />
+        {children}
+        <Footer />
+      </section>
+    </main>
+  </>
 );
 
 export default MainContainer;
