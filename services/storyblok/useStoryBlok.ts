b902f473dedf802cca9a93d3e0376dd153fb1ab2
wdev733 <53801750+wdev733@users.noreply.github.com>
2023-05-18 01:55:41 -0400

    fix blog detail page 404 error (#36)
    Alright! Blog Detail pages!

--- services/storyblok/useStoryBlok.ts
+++ services/storyblok/useStoryBlok.ts
@@ -1,14 +1,7 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 
-import { envParams } from 'shared/configs/constants';
-import StoryblokClient from 'storyblok-js-client';
-
-import type { ISbResult } from 'storyblok-js-client';
-
-const Storyblok = new StoryblokClient({
-  accessToken: envParams.storyblokAccessToken,
-});
+import { fetchBlogs } from 'services/storyblok/utils';
 
 interface useStoryBlokProps {
   getBlogDetailBySlug?: string;
@@ -19,33 +12,15 @@ export const useStoryBlok = ({ getBlogDetailBySlug }: useStoryBlokProps) => {
   const [data, setData] = useState<any[]>([]);
   const [detail, setDetail] = useState<any>();
 
-  const fetchBlogs = async () => {
+  const getBlogs = async () => {
     setLoading(true);
 
-    const blogs: any[] = [];
-    const response: ISbResult = await Storyblok.get('cdn/stories/', {
-      starts_with: 'blog/',
-      per_page: 100,
-    });
+    const blogs = await fetchBlogs();
 
-    for (const story of response.data.stories) {
-      if (story.published_at != null) {
-        if (story.full_slug.startsWith('blog/') && !story.full_slug.endsWith('blog/')) {
-          blogs.push(story);
-
-          if (getBlogDetailBySlug && story.slug === getBlogDetailBySlug) {
-            setDetail(story);
-          }
-        }
-      }
+    if (getBlogDetailBySlug) {
+      const blogDetail = blogs.find((blog) => blog.slug === getBlogDetailBySlug);
+      setDetail(blogDetail);
     }
-
-    blogs.sort((a, b) => {
-      const aTimestamp = Date.parse(a.content.published_date);
-      const bTimestamp = Date.parse(b.content.published_date);
-
-      return aTimestamp > bTimestamp ? -1 : 1;
-    });
 
     setData(blogs);
 
@@ -56,6 +31,6 @@ export const useStoryBlok = ({ getBlogDetailBySlug }: useStoryBlokProps) => {
     data,
     detail,
     isLoading,
-    fetchBlogs,
+    getBlogs,
   };
 };
--- ui/blog/BlogDetail.tsx
+++ ui/blog/BlogDetail.tsx
@@ -1,35 +1,22 @@
-import { useEffect } from 'react';
-
-import { useRouter } from 'next/router';
-import { useStoryBlok } from 'services/storyblok';
 import dateTools from 'shared/tools/date';
 import { render, NODE_IMAGE } from 'storyblok-rich-text-react-renderer';
 import style from 'ui/blog/BlogDetail.module.scss';
 import BlogHeadSeo from 'ui/blog/BlogHeadSeo';
 import SectionContainer from 'ui/common/SectionContainer';
 
-const BlogDetail = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-
-  const { fetchBlogs, detail: post, isLoading } = useStoryBlok({ getBlogDetailBySlug: slug?.toString() });
-
-  useEffect(() => {
-    fetchBlogs();
-  }, [slug]);
-
-  if (isLoading || !post) return null;
+const BlogDetail = ({ blog }) => {
+  if (!blog) return null;
 
   return (
     <>
-      <BlogHeadSeo title="Volume Finance" description="" content={post.content} />
+      <BlogHeadSeo title="Volume Finance" description="" content={blog.content} />
       <SectionContainer>
         <section className={style.container}>
-          <img className={style.image} src={`https:${post.content.image}`} />
-          <div className={style.date}>{dateTools.convertDateString2(post.content.published_date)}</div>
-          {post.content.intro && <p className={style.intro}>{post.content.intro}</p>}
+          <img className={style.image} src={`https:${blog.content.image}`} />
+          <div className={style.date}>{dateTools.convertDateString2(blog.content.published_date)}</div>
+          {blog.content.intro && <p className={style.intro}>{blog.content.intro}</p>}
           <section className={style.post}>
-            {render(post.content.long_text, {
+            {render(blog.content.long_text, {
               nodeResolvers: {
                 [NODE_IMAGE]: (children, props) => <img {...props} style={{ borderRadius: '0px', maxWidth: '100%' }} />,
               },
