89eaa205e8c931fb5992acc642f060dc3238b266
Jason Jacobs <nerfnerd@gmail.com>
2023-06-13 10:44:36 -0700

    Mixpanel add new (#46)
    * Add page loading of new site to mixpanel
    
    * needed export
    
    * minor change needed for mixpanel

--- ui/main/Main.tsx
+++ ui/main/Main.tsx
@@ -7,13 +7,15 @@ import style from 'ui/main/Main.module.scss';
 import { useEffect } from 'react';
 import Mixpanel from 'mixpanel';
 import {envParams} from "../../shared/configs/constants";
+import { useRouter } from 'next/router';
 
-const mixpanel = Mixpanel.init(envParams.storyblokAccessToken);
 
 const MainContainer = ({ children }: PropsWithChildren) => {
+    const router = useRouter();
+    const mixpanel = Mixpanel.init(envParams.storyblokAccessToken);
 
     useEffect(() => {
-        mixpanel.track('PAGE_LOAD');
+        let result = mixpanel.track('PAGE_LOAD', { page: router.pathname });
     }, []);
 
     return (
