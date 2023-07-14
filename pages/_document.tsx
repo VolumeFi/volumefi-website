792934dc9a934caf472352b0de14ea39d5b014b4
Jason Jacobs <nerfnerd@gmail.com>
2023-07-14 16:31:06 -0700

    update for build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,10 @@
+import { Mixpanel } from 'mixpanel-react-native';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import { Mixpanel } from 'mixpanel-react-native';
+
 
 const trackAutomaticEvents = true;
-const mixpanel = new Mixpanel("009859534ebc340a92901d2d252f4f94", trackAutomaticEvents);
+const mixpanel = new Mixpanel('009859534ebc340a92901d2d252f4f94', trackAutomaticEvents);
 
 mixpanel.init();
 
