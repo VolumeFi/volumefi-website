4324f9697d3f3d93e67610801a1f65ead1a2b785
Taariq Lewis <701864+taariq@users.noreply.github.com>
2025-07-10 08:01:48 -0700

    Update team (#65)
    * removed old team
    
    * fix npm issues
    
    * finish team update

--- shared/configs/teams.ts
+++ shared/configs/teams.ts
@@ -15,27 +15,7 @@ export const teamMembers: TeamMember[] = [
     title: 'Senior Backend Engineer',
     photo: '/assets/teams/Christian.png',
   },
-  {
-    name: 'Steven Jung',
-    title: 'Senior Backend Engineer',
-    photo: '/assets/teams/Steven.png',
-  },
-  {
-    name: 'William Chang',
-    title: 'Senior Backend Engineer',
-    photo: '/assets/teams/William.png',
-  },
-  {
-    name: 'Jack Lin',
-    title: 'Senior Backend Engineer',
-    photo: '/assets/teams/Jacky.png',
-  },
-  {
-    name: 'James Lee',
-    title: 'Senior Front-end Engineer',
-    photo: '/assets/teams/James.png',
-  },
-  {
+   {
     name: 'Michael Arief',
     title: 'AI Researcher',
     photo: '/assets/teams/Michael.png',
